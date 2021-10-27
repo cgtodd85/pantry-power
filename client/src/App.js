b3ce5408c113486f89a5d5924b00c40b71806fe4
Connor Todd <cgtodd85@gmail.com>
2021-10-27 00:15:08 -0400

    react-bootstrap and initial router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigator from "./components/Navigator";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return <div className="App">hi, what up</div>;
+  return (
+    <Router>
+      <Navigator />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navigator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+
+function Navigator() {
+  return (
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/dashboard">Dashboard</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default Navigator;
