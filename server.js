31c71ad17eee9eeee0943ae1fedb691fa651190a
Connor Todd <cgtodd85@gmail.com>
2021-10-27 17:23:56 -0400

    express session details in server

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
-
+const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 9001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "this is a secret",
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store,
+};
 
 // Connect to the Mongo DB
 mongoose.connect(
@@ -25,7 +23,17 @@ mongoose.connect(
     useFindAndModify: false,
   }
 );
+// Define middleware here
+app.use(session(sessionOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
+// Add routes, both API and view
+app.use(routes);
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
