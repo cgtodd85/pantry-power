18082d8f0871e249a21b261fe7b90c65ed204f0e
Connor Todd <cgtodd85@gmail.com>
2021-10-31 15:12:30 -0400

    added folders, created user model

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is required",
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "password is required!",
+    validate: [({ length }) => length >= 4, "Password should be longer"],
+  },
+  userCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.pre("updateOne", function (next) {
+  const update = this.getUpdate().$set;
+  if (!update.password) {
+    return next();
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(update.password, 10);
+    update.password = hashedPassword;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = function (pass) {
+  return bcrypt.compareSync(pass, this.password);
+};
+
+const User = mongoose.model(User, userSchema);
+
+module.exports = User;
