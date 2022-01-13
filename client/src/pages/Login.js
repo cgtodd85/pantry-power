56c9a9c2caebf9a6628aa29ba0b8704b88e539c0
Connor Todd <cgtodd85@gmail.com>
2022-01-13 18:13:25 -0500

    update to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,9 +4,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import API from "../utils/API";
 
 function Login() {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
   const [formObject, setFormObject] = useState({});
 
   function handleInputChange(event) {
@@ -36,19 +37,32 @@ function Login() {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Username</Form.Label>
-              <Form.Control name="username" placeholder="Enter username" />
+              <Form.Control
+                onChange={handleInputChange}
+                name="username"
+                placeholder="Enter username"
+              />
               <Form.Text className="text-muted">anything</Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control name="password" placeholder="Password" />
+              <Form.Control
+                onChange={handleInputChange}
+                name="password"
+                placeholder="Password"
+              />
               <Form.Text className="text-muted">minimum 3 characters</Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check if you are a robot" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button
+              disabled={!(formObject.author && formObject.title)}
+              onClick={handleFormSubmit}
+              variant="primary"
+              type="submit"
+            >
               Submit
             </Button>
           </Form>
