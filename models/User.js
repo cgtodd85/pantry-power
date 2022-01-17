c3f7c78280ff7ac02b85496cf42bd39e03d9bef3
Connor Todd <cgtodd85@gmail.com>
2022-01-17 12:44:59 -0500

    updating user model

--- models/User.js
+++ models/User.js
@@ -1,39 +1,40 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: "Username is required",
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: "password is required!",
-    validate: [({ length }) => length >= 4, "Password should be longer"],
-  },
-  userCreated: {
-    type: Date,
-    default: Date.now,
-  },
-});
+const userSchema = new Schema({});
 
-userSchema.pre("save", function (next) {
-  const user = this;
-  if (!user.isModified("password")) {
-    return next();
-  }
-  try {
-    const hashedPassword = bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
-    next();
-  } catch (err) {
-    return next(err);
-  }
-});
+// username: {
+//     type: String,
+//     trim: true,
+//     required: "Username is required",
+//   },
+//   password: {
+//     type: String,
+//     trim: true,
+//     required: "password is required!",
+//     validate: [({ length }) => length >= 4, "Password should be longer"],
+//   },
+//   userCreated: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// userSchema.pre("save", function (next) {
+//   const user = this;
+//   if (!user.isModified("password")) {
+//     return next();
+//   }
+//   try {
+//     const hashedPassword = bcrypt.hash(user.password, 10);
+//     user.password = hashedPassword;
+//     next();
+//   } catch (err) {
+//     return next(err);
+//   }
 
 // userSchema.pre("updateOne", function (next) {
 //   const update = this.getUpdate().$set;
@@ -49,10 +50,10 @@ userSchema.pre("save", function (next) {
 //   }
 // });
 
-userSchema.methods.comparePassword = function (pass) {
-  return bcrypt.compareSync(pass, this.password);
-};
+// userSchema.methods.comparePassword = function (pass) {
+//   return bcrypt.compareSync(pass, this.password);
+// };
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
