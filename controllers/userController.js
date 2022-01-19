2d688af0eac9b38b081351dae533a661618f64fe
Connor Todd <cgtodd85@gmail.com>
2022-01-19 17:20:06 -0500

    updating user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,125 @@
-const db = require("../models");
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
-module.exports = {
-  findAll: function (req, res) {
-    db.User.find(req.query)
-      .sort({ date: -1 })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  findById: function (req, res) {
-    db.User.findById(req.params.id)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  create: function (req, res) {
-    db.User.create(req.body)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  update: function (req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  remove: function (req, res) {
-    db.User.findById({ _id: req.params.id })
-      .then((dbModel) => dbModel.remove())
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-};
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+//@description     Register new user
+//@route           POST /api/users/
+//@access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(404);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
+// @desc    GET user profile
+// @route   GET /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.pic = req.body.pic || user.pic;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      pic: updatedUser.pic,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export { authUser, updateUserProfile, registerUser };
+
+// const db = require("../models");
+
+// module.exports = {
+//   findAll: function (req, res) {
+//     db.User.find(req.query)
+//       .sort({ date: -1 })
+//       .then((dbModel) => res.json(dbModel))
+//       .catch((err) => res.status(422).json(err));
+//   },
+//   findById: function (req, res) {
+//     db.User.findById(req.params.id)
+//       .then((dbModel) => res.json(dbModel))
+//       .catch((err) => res.status(422).json(err));
+//   },
+//   create: function (req, res) {
+//     db.User.create(req.body)
+//       .then((dbModel) => res.json(dbModel))
+//       .catch((err) => res.status(422).json(err));
+//   },
+//   update: function (req, res) {
+//     db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+//       .then((dbModel) => res.json(dbModel))
+//       .catch((err) => res.status(422).json(err));
+//   },
+//   remove: function (req, res) {
+//     db.User.findById({ _id: req.params.id })
+//       .then((dbModel) => dbModel.remove())
+//       .then((dbModel) => res.json(dbModel))
+//       .catch((err) => res.status(422).json(err));
+//   },
+// };
